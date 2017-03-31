cb05d590d18a6065f9a25e7b44d4ddd3876b2691
SamMichie <sammichie@hotmail.co.uk>
2017-03-31 14:53:40 +0100

    adding js file
    adding js file with functions for authentication and link from js to
    html

---
+++ week1/testjs.js
@@ -0,0 +1,36 @@
+function go(){
+  console.log('Sign-in state: ');
+  var additionalParams = {
+    'callback': signinCallback
+  };
+
+  var additionalParams = {
+    'callback': signinCallback
+  };
+
+  var signinButton = document.getElementById('signinButton');
+  signinButton.addEventListener('click', function(){
+    console.log('Sign-in state: ');
+    gapi.auth.signIn(additionalParams);
+  });
+}
+
+function signinCallback(authResult){
+  if(authResult['status']['signed_in']){
+    document.getElementById('signinButton').setAttributes('style', 'display: none');
+    gapi.auth.setToken(authResult);
+    gapi.client.load('plus', 'v1').then(function(){
+      var request = gapi.client.plus.people.get({
+        'userId' : 'me'
+      });
+
+      request.execute(function(resp){
+        document.getElementById('result').innerHTML =
+        "<h2> Hello " +resp.displayName+"</h2>"+
+        "<p>"+resp.id+"</p>";
+      });
+    });
+  } else{
+    console.log('Sign-in state: ' + authResult['error']);
+  }
+}
